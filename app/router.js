9b2de9fab26b4a0f110ec6639495032ab8886e3c
Gavin Davies <gavin.davies@derivco.co.uk>
2017-08-09 15:17:36 +0100

    Intermediate checkin

--- app/router.js
+++ app/router.js
@@ -10,11 +10,24 @@ Router.map(function() {
   this.route('pages');
   this.route('about');
   this.route('enter');
-  this.route('team-declaration');  
+  this.route('team-declaration');
   this.route('gallery');
   this.route('results');
   this.route('page', {path: 'page/:page_slug'});
-  
+
+  this.route('ekiden', function() {
+    this.route('about');
+    this.route('enter');
+    this.route('declaration');
+    this.route('course');
+    this.route('results');
+    this.route('gallery');
+  });
+  this.route('cross-country', function() {
+    this.route('about');
+    this.route('results');
+    this.route('gallery');
+  });
 });
 
 export default Router;
