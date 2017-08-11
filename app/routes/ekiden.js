3b69d278e9489bfba31636b235517faf1cc4d6f5
Gavin Davies <gavin.davies@derivco.co.uk>
2017-08-11 10:58:25 +0100

    Move to ember-MDL in place of paper

--- app/routes/ekiden.js
+++ app/routes/ekiden.js
@@ -1,22 +1,22 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({	  
-	model: function() {
-	var categoryId =  80; // TODO look up.
+export default Ember.Route.extend({    
+  model: function() {
+  var categoryId =  80; // TODO look up.
     return Ember.RSVP.hash({
       about: this.store.query('post', {
-			slug: 'the-race'
-		}),
+      slug: 'the-race'
+    }),
       enter: this.store.query('post', {
-			slug: 'enter'
-		}),
-	  pages: this.store.query('page', {
-			parent: '4167'
-		}),
-	  posts: this.store.query('post', {
-			per_page: 99,
-			categories: categoryId
-		})
+      slug: 'enter'
+    }),
+    pages: this.store.query('page', {
+      parent: '4167'
+    }),
+    posts: this.store.query('post', {
+      per_page: 99,
+      categories: categoryId
+    })
     });
   },
 });
--- config/environment.js
+++ config/environment.js
@@ -1,24 +1,20 @@
-/* eslint-env node */
-'use strict';
+/* jshint node: true */
 
 module.exports = function(environment) {
-  let ENV = {
+  var ENV = {
     modulePrefix: 'ember-ekiden',
-    environment,
+    environment: environment,
     rootURL: '/',
     locationType: 'auto',
-	wordpressHost: 'http://test.ipswichjaffa.org.uk',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
       }
     },
-
+    sassOptions: {
+      includePaths: ['bower_components/material-design-lite/src']
+    },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
@@ -38,6 +34,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
